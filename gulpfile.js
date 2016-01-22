00dc4bc4e46b5069dcd3b9a43520f21208c34caf
hikouki <ame.kh000@gmail.com>
2016-01-22 10:37:10 +0900

    Add release task for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require("gulp-uglify");
 
-gulp.task('build.min', function(){
+gulp.task('build.min', function() {
     browserify({
         entries: [
             'src/qitalk.js',
@@ -18,7 +18,7 @@ gulp.task('build.min', function(){
         .pipe(gulp.dest('./dict'));
 });
 
-gulp.task('build.plain', function(){
+gulp.task('build.plain', function() {
     browserify({
         entries: [
             'src/qitalk.js',
@@ -28,6 +28,30 @@ gulp.task('build.plain', function(){
         .pipe(source('qitalk.js'))
         .pipe(buffer())
         .pipe(gulp.dest('./dict'));
+});
+
+gulp.task('release', function() {
+    browserify({
+        entries: [
+            'src/qitalk.js',
+        ],
+        debug: false
+    })
+        .bundle()
+        .pipe(source('qitalk.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./release'));
+
+    browserify({
+        entries: [
+            'src/qitalk.js',
+        ]
+    })
+        .bundle()
+        .pipe(source('qitalk.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('./release'));
 });
 
 gulp.task('default', ['build.plain', 'build.min']);
